a411b3fa265b439d2e0136081eea14bc176672e4
Glenn Dwiyatcita <dwiyatci@mail.com>
2018-05-17 16:37:08 +0100

    ✨ feat(webpack 4): [WIP] try to make it work with webpack 4

--- src/app.js
+++ src/app.js
@@ -4,6 +4,21 @@
 
 /* global angular */
 
+import 'babel-polyfill';
+
+/**
+ * Cumulocity UI core SDK (8.x).
+ */
+import 'cumulocity-clients-javascript/build/main-standalone';
+
+/**
+ * Other vendor dependencies for this amazing project.
+ */
+import 'bootstrap-loader';
+import 'angular-route';
+import 'angular-ui-bootstrap/dist/ui-bootstrap';
+import 'angular-ui-bootstrap/dist/ui-bootstrap-tpls';
+
 import './app.css';
 import appModule from './app.module';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,147 +1,148 @@
 /**
  * Created by glenn on 07.05.17.
- * Last updated on 31.10.17.
+ * Last updated on 17.05.18.
  */
 
 const { resolve } = require('path');
 
-const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const config = {
-  entry: {
-    app: './src/app.js',
-    vendor: [
-      /**
-       * Cumulocity UI core SDK (8.x).
-       */
-      'cumulocity-clients-javascript/build/main-standalone.js',
+// const config = {
+//   entry: {
+//     app: './src/app.js',
+//     vendor: [
+//       /**
+//        * Cumulocity UI core SDK (8.x).
+//        */
+//       'cumulocity-clients-javascript/build/main-standalone.js',
+//
+//       /**
+//        * Other vendor dependencies for this amazing project.
+//        */
+//       'bootstrap-loader',
+//       'angular-route',
+//       'angular-ui-bootstrap/dist/ui-bootstrap.js',
+//       'angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
+//     ],
+//     polyfills: [
+//       'babel-polyfill',
+//     ],
+//   },
+//   resolve: {
+//     modules: [
+//       'node_modules',
+//       resolve(__dirname, 'src'),
+//     ],
+//   },
+// };
 
-      /**
-       * Other vendor dependencies for this amazing project.
-       */
-      'bootstrap-loader',
-      'angular-route',
-      'angular-ui-bootstrap/dist/ui-bootstrap.js',
-      'angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
-    ],
-    polyfills: [
-      'babel-polyfill',
-    ],
-  },
-  output: {
-    filename: ifProd('[name].[chunkhash].js', '[name].js'),
-    path: resolve(__dirname, 'assets'),
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /cumulocity-clients-javascript/,
-        use: 'script-loader',
-      },
-      // Bootstrap 3
-      {
-        test: /bootstrap-sass\/assets\/javascripts\//,
-        use: 'imports-loader?jQuery=>window.jQuery',
-      },
-      {
-        test: /\.js$/,
-        include: [
-          resolve(__dirname, 'src'),
-        ],
-        use: [
-          {
-            // https://webpack.js.org/loaders/babel-loader
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true,
+module.exports = (env) => {
+  const config = {
+    mode: eitherDevOrProd('development', 'production'),
+    entry: './src/app.js',
+    output: {
+      filename: eitherDevOrProd('[name].js', '[name].[chunkhash].js'),
+      path: resolve(__dirname, 'assets'),
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /cumulocity-clients-javascript/,
+          use: 'script-loader',
+        },
+        // Bootstrap 3
+        {
+          test: /bootstrap-sass\/assets\/javascripts\//,
+          use: 'imports-loader?jQuery=>window.jQuery',
+        },
+        {
+          test: /\.js$/,
+          include: [
+            resolve(__dirname, 'src'),
+          ],
+          use: [
+            {
+              // https://webpack.js.org/loaders/babel-loader
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+              },
             },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({ use: 'css-loader' }),
-      },
-      {
-        test: /\.json$/,
-        use: 'json-loader',
-      },
-      {
-        test: /\.html$/,
-        use: 'html-loader',
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: 'url-loader?limit=8192',
-      },
-      {
-        test: /\.(woff2?|svg)$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot)$/,
-        use: 'file-loader',
-      },
-    ],
-  },
-  plugins: [
-    // Code Splitting - CSS
-    new ExtractTextPlugin(ifProd('[name].[chunkhash].css', '[name].css')),
-
-    // Code Splitting - Libraries
-    new webpack.optimize.CommonsChunkPlugin({
-      // https://stackoverflow.com/q/39548175/2013891
-      // The order of this array matters.
-      names: [
-        'polyfills',
-        'vendor',
-        'manifest',
+          ],
+        },
+        {
+          test: /\.json$/,
+          type: 'json',
+        },
+        {
+          test: /\.css$/,
+          // use: ['style-loader', 'css-loader'],
+          use: ExtractTextPlugin.extract({
+            use: 'css-loader',
+            fallback: 'style-loader',
+          }),
+          // use: [
+          //   MiniCssExtractPlugin.loader,
+          //   'css-loader',
+          // ],
+        },
+        {
+          test: /\.html$/,
+          use: 'html-loader',
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: 'url-loader?limit=8192',
+        },
+        {
+          test: /\.(woff2?|svg)$/,
+          use: 'url-loader?limit=10000',
+        },
+        {
+          test: /\.(ttf|eot)$/,
+          use: 'file-loader',
+        },
       ],
-    }),
+    },
+    plugins: [
+      // Code Splitting - CSS
+      new ExtractTextPlugin(eitherDevOrProd('[name].css', '[name].[chunkhash].css')),
+      // new MiniCssExtractPlugin({
+      //   filename: eitherDevOrProd('[name].css', '[name].[chunkhash].css'),
+      // }),
 
-    // Caching
-    new HtmlWebpackPlugin({ template: './src/index.ejs' }),
-    new InlineManifestWebpackPlugin(),
-
-    ...ifProd(
-      [
-        // Building for Production
-        new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
-      ],
-      [
-        new webpack.NoEmitOnErrorsPlugin(),
-
-        // HMR
-        new webpack.HotModuleReplacementPlugin(),
-      ]
-    ),
-
-    // Scope hoisting
-    new webpack.optimize.ModuleConcatenationPlugin(),
-  ],
-  devtool: ifProd('source-map', 'eval'),
-  resolve: {
-    modules: [
-      'node_modules',
-      resolve(__dirname, 'src'),
+      // Caching
+      new HtmlWebpackPlugin({ template: './src/index.ejs' }),
     ],
-  },
-  devServer: {
-    contentBase: resolve(__dirname, 'assets'),
-    compress: true,
-    noInfo: true,
-    historyApiFallback: true,
-    hot: true,
-    https: true,
-  },
+    optimization: {
+      // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+      splitChunks: {
+        chunks: 'all',
+        // name: false,
+      },
+      runtimeChunk: true,
+    },
+    resolve: {
+      modules: [
+        resolve(__dirname, 'src'),
+        'node_modules',
+      ],
+    },
+    devServer: {
+      contentBase: resolve(__dirname, 'assets'),
+      compress: true,
+      noInfo: false,
+      historyApiFallback: true,
+      https: true,
+    },
+  };
+
+  return config;
+
+  function eitherDevOrProd(devStuff, prodStuff) {
+    return (env && env.production) ? prodStuff : devStuff;
+  }
 };
-
-function ifProd(prodStuff, devStuff) {
-  return (process.env.NODE_ENV === 'production') ? prodStuff : devStuff;
-}
-
-module.exports = config;
